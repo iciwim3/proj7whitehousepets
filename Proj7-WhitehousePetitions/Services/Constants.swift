27abe7f6fe1045219e6bb4eb1cf76748022d2cee
SKE <appybuildmore@gmail.com>
2018-07-12 13:58:05 -0700

    Made ViewController a subclass of UITableViewController, deleted the VC on Storyboard and added a TableViewController, changed the class to ViewController, designed the cell of the prototype cell, added a Constants file for identifiers and segues.

---
+++ Proj7-WhitehousePetitions/Services/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Proj7-WhitehousePetitions
+//
+//  Created by Sain-R Edwards Jr. on 7/12/18.
+//  Copyright © 2018 Swift Koding 4 Everyone. All rights reserved.
+//
+
+import Foundation
+
+// Cell Identifiers
+let TABLE_VIEW_CELL = "Cell"
