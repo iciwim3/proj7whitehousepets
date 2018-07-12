8b4b925c06138695c5d3ea74b0f21f1a0051090d
SKE <appybuildmore@gmail.com>
2018-07-12 13:35:48 -0700

    Initial Commit

---
+++ Proj7-WhitehousePetitions/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Proj7-WhitehousePetitions
+//
+//  Created by Sain-R Edwards Jr. on 7/12/18.
+//  Copyright © 2018 Swift Koding 4 Everyone. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
