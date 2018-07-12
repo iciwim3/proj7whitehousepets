4dfeee8ccea6c6e23adf1b9f2a16cfea0cc4097b
SKE <appybuildmore@gmail.com>
2018-07-12 14:41:54 -0700

    Added code and comments to the ViewController to parse the data from the WhiteHouse.gov petition site and converted to SwiftyJSON. Updated cellForRowAt method so the titles and subtitles will appear as they should from the site.

--- Proj7-WhitehousePetitions/ViewController.swift
+++ Proj7-WhitehousePetitions/ViewController.swift
@@ -10,11 +10,38 @@ import UIKit
 
 class ViewController: UITableViewController {
     
-    var petitions = [String]()
+    var petitions = [[String: String]]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Points to the Whitehouse.gov server, accessing petitions
+        let urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
+        
+        // Use if let to make sure URL is valid
+        if let url = URL(string: urlString) {
+            // Created a string object using 'contentsOf' method
+            if let data = try? String(contentsOf: url) {
+                // If String object created successfully, create a new JSON object from it. This is SwiftyJSON structure
+                let json = JSON(parseJSON: data)
+                
+                if json["metadata"]["responseInfo"]["status"].intValue == 200 {
+                    parse(json: json)
+                }
+            }
+        }
+        
+    }
+    
+    func parse(json: JSON) {
+        for result in json["results"].arrayValue {
+            let title = result["title"].stringValue
+            let body = result["body"].stringValue
+            let sigs = result["signatureCount"].stringValue
+            let obj = ["title": title, "body": body, "sigs": sigs]
+            petitions.append(obj)
+        }
+        tableView.reloadData()
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -23,9 +50,22 @@ class ViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: TABLE_VIEW_CELL, for: indexPath)
-        cell.textLabel?.text = "Title goes here"
-        cell.detailTextLabel?.text = "Subtitle goes here"
+        let petition = petitions[indexPath.row]
+        cell.textLabel?.text = petition["title"]
+        cell.detailTextLabel?.text = petition["body"]
         return cell
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
