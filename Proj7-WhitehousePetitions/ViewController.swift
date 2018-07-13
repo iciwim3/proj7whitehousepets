e4708ebe27d9c818b67b5f404c721ece184b60b7
SKE <appybuildmore@gmail.com>
2018-07-13 11:43:48 -0500

    Created DetailVC class and placed html info in the class. Connected the ViewController to the DetailVC by using didSelectRowAt method.

--- Proj7-WhitehousePetitions/ViewController.swift
+++ Proj7-WhitehousePetitions/ViewController.swift
@@ -55,6 +55,12 @@ class ViewController: UITableViewController {
         cell.detailTextLabel?.text = petition["body"]
         return cell
     }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let vc = DetailVC()
+        vc.detailItem = petitions[indexPath.row]
+        navigationController?.pushViewController(vc, animated: true)
+    }
 }
 
 
